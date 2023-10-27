37b3e34cda5e2e28b8dcc66ebc172d3dddd3051c
victor <victor.murithi@student.moringaschool.com>
2023-10-27 11:57:15 +0300

    updated tasklist and task js

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
 
 import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [tasks, setTasks] = useState(TASKS);
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  const filterTasks = (category) => {
+    setSelectedCategory(category);
+  };
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter categories={CATEGORIES} filterTasks={filterTasks} />
+      <NewTaskForm categories={CATEGORIES} tasks={tasks} setTasks={setTasks} />
+      <TaskList tasks={tasks} selectedCategory={selectedCategory} />
     </div>
   );
 }
