1ca38a51cb35f5513d202fc12d6a3bbc1f1fe057
victor <victor.murithi@student.moringaschool.com>
2023-10-27 12:05:07 +0300

    added filter logic

--- src/components/App.js
+++ src/components/App.js
@@ -9,16 +9,20 @@ function App() {
   const [tasks, setTasks] = useState(TASKS);
   const [selectedCategory, setSelectedCategory] = useState("All");
 
+
   const filterTasks = (category) => {
     setSelectedCategory(category);
   };
+  const filteredTasks = selectedCategory === "All"
+    ? tasks
+    : tasks.filter((task) => task.category === selectedCategory);
 
   return (
     <div className="App">
       <h2>My tasks</h2>
       <CategoryFilter categories={CATEGORIES} filterTasks={filterTasks} />
       <NewTaskForm categories={CATEGORIES} tasks={tasks} setTasks={setTasks} />
-      <TaskList tasks={tasks} selectedCategory={selectedCategory} />
+      <TaskList tasks={filteredTasks} selectedCategory={selectedCategory}/>
     </div>
   );
 }
